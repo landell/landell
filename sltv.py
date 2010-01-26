7d6a6d23c6ed2874627db196567a586375077405
Luciana Fujii Pontello <luciana@holoscopio.com>
2010-01-25 22:45:37 -0200

    Pressing play disables stop and vice-versa

--- sltv.py
+++ sltv.py
@@ -9,14 +9,16 @@ import gtk
 class Sltv_Overlay:
 
 	def __init__(self):
+		self.state = 0
 		self.interface = gtk.Builder()
-		self.interface.add_from_file("gui-mockup1.glade")
+		self.interface.add_from_file("sltv.ui")
 		window = self.interface.get_object("window1")
 		window.show_all()
 
 		file_location_entry = self.interface.get_object("file_location_entry")
 		play_button = self.interface.get_object("play_button")
 		stop_button = self.interface.get_object("stop_button")
+		stop_button.set_active(True)
 		overlay_button = self.interface.get_object("overlay_button")
 
 		play_button.connect("toggled", self.on_play_press)
@@ -25,27 +27,33 @@ class Sltv_Overlay:
 
 
 	def on_play_press(self, signal):
-		overlay_text_entry = self.interface.get_object("overlay_text_entry")
-		overlay_text = overlay_text_entry.get_text()
-		self.player = gst.Pipeline("player")
-		self.source = gst.element_factory_make("v4l2src", "source")
-		self.overlay = gst.element_factory_make("cairotextoverlay", "overlay")
-		self.sink = gst.element_factory_make("xvimagesink", "sink")
-		self.player.add(self.source, self.overlay, self.sink);
-		gst.element_link_many(self.source, self.overlay, self.sink)
+		if (self.state == 0):
+			stop_button = self.interface.get_object("stop_button")
+			stop_button.set_active(False)
+			self.state = 1
+			overlay_text_entry = self.interface.get_object("overlay_text_entry")
+			overlay_text = overlay_text_entry.get_text()
+			self.player = gst.Pipeline("player")
+			self.source = gst.element_factory_make("v4l2src", "source")
+			self.overlay = gst.element_factory_make("cairotextoverlay", "overlay")
+			self.sink = gst.element_factory_make("xvimagesink", "sink")
+			self.player.add(self.source, self.overlay, self.sink);
+			gst.element_link_many(self.source, self.overlay, self.sink)
 
-		self.overlay.set_property("text", overlay_text)
+			self.overlay.set_property("text", overlay_text)
 
-		self.player.set_state(gst.STATE_PLAYING);
+			self.player.set_state(gst.STATE_PLAYING);
 	
-		bus = self.player.get_bus()
-		bus.add_signal_watch()
-		bus.connect("message", self.on_message)
+			bus = self.player.get_bus()
+			bus.add_signal_watch()
+			bus.connect("message", self.on_message)
 
 	def on_stop_press(self, signal):
-
-		self.player.set_state(gst.STATE_NULL);
-
+		if (self.state == 1):
+			self.player.set_state(gst.STATE_NULL);
+			play_button = self.interface.get_object("play_button")
+			play_button.set_active(False)
+			self.state = 0
 
 	def on_overlay_change(self,signal):
 
