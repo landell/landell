e1f5ecd59beeb63c325b0d9ab06ec4a4ae1059d3
Luciana Fujii Pontello <luciana@holoscopio.com>
2010-02-04 20:57:07 -0200

    Included effect enabling and disabling
    Effects change don't work during transmission

--- sltv.py
+++ sltv.py
@@ -29,6 +29,7 @@ from preview import *
 from audio import *
 from preview import *
 from effects import *
+from swap import *
 
 def show_output(menuitem, output):
 	output.show_window()
@@ -44,12 +45,13 @@ def create_effects_combobox(combobox):
 	combobox.add_attribute(cell, 'text', 0)
 	for type in Effect.get_types():
 		liststore.append((type,))
-	combobox.set_active(1)
+	combobox.set_active(0)
 
 class Sltv:
 
 	def __init__(self):
 		self.state = "stopped"
+		self.player = None
 		self.interface = gtk.Builder()
 		self.interface.add_from_file("sltv.ui")
 		window = self.interface.get_object("window1")
@@ -68,14 +70,17 @@ class Sltv:
 		encoding_menuitem = self.interface.get_object("encoding_menuitem")
 		self.effect_combobox = self.interface.get_object("effect_combobox")
 		create_effects_combobox(self.effect_combobox)
+		self.effect_checkbutton = self.interface.get_object("effect_checkbutton")
+		self.effect_label = self.interface.get_object("effect_label")
+		self.set_effects(False)
 
+		self.effect_checkbutton.connect("toggled", self.effect_toggled)
 		play_button.connect("toggled", self.on_play_press)
 		stop_button.connect("toggled", self.on_stop_press)
 		overlay_button.connect("pressed", self.on_overlay_change)
 		window.connect("delete_event", self.on_window_closed)
 		output_menuitem.connect("activate", show_output, self.output)
 		encoding_menuitem.connect("activate", show_encoding, self.encoding)
-
 
 	def on_play_press(self, event):
 		if (self.state == "stopped"):
@@ -93,25 +98,34 @@ class Sltv:
 
 			self.player = gst.Pipeline("player")
 			self.videosrc = gst.element_factory_make("v4l2src", "videosrc")
-			self.effect = Effect.make_effect(self.effect_combobox.get_active_text())
 			self.overlay = gst.element_factory_make("textoverlay", "overlay")
 			self.tee = gst.element_factory_make("tee", "tee")
 			queue1 = gst.element_factory_make("queue", "queue1")
 			queue2 = gst.element_factory_make("queue", "queue2")
-			queue3 = gst.element_factory_make("queue", "queue3")
+			self.queue3 = gst.element_factory_make("queue", "queue3")
 			queue4 = gst.element_factory_make("queue", "queue4")
 			self.mux = self.encoding.get_mux()
 			self.sink = self.output.get_output()
 			self.preview_element = self.preview.get_preview()
 			self.audiosrc = self.audio.get_audiosrc()
+			self.colorspace = gst.element_factory_make("ffmpegcolorspace", "colorspacesink")
+			if self.effect_enabled:
+				self.effect = Effect.make_effect(self.effect_combobox.get_active_text())
+				self.effect_name = self.effect_combobox.get_active_text()
+				self.player.add(self.effect)
 			self.player.add(self.videosrc, self.overlay, self.tee, queue1,
-					queue3, self.mux, queue2, self.preview_element, self.sink,
-					self.audiosrc, queue4, self.effect)
-			err = gst.element_link_many(self.videosrc, queue3, self.effect, self.overlay, self.tee, queue1,
-					self.mux, self.sink)
+					self.queue3, self.mux, self.sink,
+					self.audiosrc, queue4, self.colorspace)
+			self.videosrc.link(self.queue3)
+			if self.effect_enabled:
+				gst.element_link_many(self.queue3, self.effect, self.overlay)
+			else:
+				self.queue3.link(self.overlay)
+			#self.player.add(queue2, self.preview_element)
+			err = gst.element_link_many(self.overlay, self.tee, queue1, self.colorspace, self.mux, self.sink)
 			if err == False:
 				print "Error conecting elements"
-			err = gst.element_link_many(self.tee, queue2, self.preview_element)
+			#err = gst.element_link_many(self.tee, queue2, self.preview_element)
 			gst.element_link_many(self.audiosrc, queue4, self.mux)
 			if err == False:
 				print "Error conecting preview"
@@ -124,6 +138,14 @@ class Sltv:
 			bus.connect("message", self.on_message)
 			bus.connect("sync-message::element", self.on_sync_message)
 			self.player.set_state(gst.STATE_PLAYING)
+
+	def set_effects(self, state):
+		self.effect_combobox.set_sensitive(state)
+		self.effect_label.set_sensitive(state)
+		self.effect_enabled = state
+
+	def effect_toggled(self, checkbox):
+		self.set_effects(not self.effect_enabled)
 
 	def on_stop_press(self, event):
 		if (self.state == "playing"):
