3a64c5d521f7ee2975a7cf09d8fb31350916e96f
Marcelo Jorge Vieira (metal) <metal@alucinados.com>
2010-02-09 17:52:05 -0200

    Cosmetic changes (code style)

--- about.py
+++ about.py
@@ -34,23 +34,24 @@ class About:
 		self.about.set_wrap_license(True)
 		self.about.set_license(
 			"This program is free software; you can redistribute it and/or modify"
-	 		"it under the terms of the GNU General Public License as published by"
-		 	"the Free Software Foundation; either version 2 of the License, or"
-		 	"(at your option) any later version."
+			"it under the terms of the GNU General Public License as published by"
+			"the Free Software Foundation; either version 2 of the License, or"
+			"(at your option) any later version."
 
-		 	"This program is distributed in the hope that it will be useful,"
-		 	"but WITHOUT ANY WARRANTY; without even the implied warranty of"
-		 	"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
-		 	"GNU General Public License for more details."
+			"This program is distributed in the hope that it will be useful,"
+			"but WITHOUT ANY WARRANTY; without even the implied warranty of"
+			"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
+			"GNU General Public License for more details."
 
-		 	"You should have received a copy of the GNU General Public License along"
-	 	"with this program; if not, write to the Free Software Foundation, Inc.,"
-	 	"51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA."
+			"You should have received a copy of the GNU General Public License along"
+			"with this program; if not, write to the Free Software Foundation, Inc.,"
+			"51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA."
 		)
-		authors = ["Luciana Fujii Pontello <luciana@holoscopio.com>",
-				"Marcelo Jorge Vieira <marcelo@holoscopio.com>",
-				"Thadeu Lima de Souza Cascardo <cascardo@holoscopio.com>"
-				]
+		authors = [
+			"Luciana Fujii Pontello <luciana@holoscopio.com>",
+			"Marcelo Jorge Vieira <marcelo@holoscopio.com>",
+			"Thadeu Lima de Souza Cascardo <cascardo@holoscopio.com>"
+		]
 		self.about.set_authors(authors)
 
 	def show_window(self):
