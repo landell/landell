b728358e1fc5ea85c5c3134318d51f26d09224ce
Marcelo Jorge Vieira (metal) <metal@alucinados.com>
2010-02-09 18:12:10 -0200

    Added website in About dialog

--- about.py
+++ about.py
@@ -31,6 +31,7 @@ class About:
 		self.about.set_name("SLTV")
 		self.about.set_copyright("Copyright 2010 Holoscopio Tecnologia")
 		self.about.set_version("0.1")
+		self.about.set_website("https://wiki.softwarelivre.org/TV/ProjetoSLTV")
 		self.about.set_wrap_license(True)
 		self.about.set_license(
 			"This program is free software; you can redistribute it and/or modify"
