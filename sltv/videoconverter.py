37238e52c2abd2649f92745cd30dca11229d3b5b
Luciana Fujii Pontello <luciana@holoscopio.com>
2010-03-11 19:09:05 -0300

    Added videoconverter to be used before encoding
    Outputs will have a fixed resolution, despite video switching. This
    videoconverter will convert the video to the right resolution.

---
+++ sltv/videoconverter.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2010 Holoscopio Tecnologia
+# Author: Luciana Fujii Pontello <luciana@holoscopio.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import gobject
+import pygst
+pygst.require("0.10")
+import gst
+
+class VideoConverter(gst.Bin):
+
+    def __init__(self, type):
+        gst.Bin.__init__(self)
+        self.colorspace = gst.element_factory_make(
+                "ffmpegcolorspace", "videoconvert_colorspace"
+        )
+        self.add(self.colorspace)
+
+        self.videoscale = gst.element_factory_make(
+                "videoscale", "videoconvert_videoscale"
+        )
+        self.add(self.videoscale)
+
+        self.capsfilter = gst.element_factory_make(
+                "capsfilter", "videoconvert_capsfilter"
+        )
+        self.add(self.capsfilter)
+
+        gst.element_link_many(
+                self.colorspace, self.videoscale, self.capsfilter
+        )
+
+        self.source_pad = gst.GhostPad(
+                "src", self.capsfilter.src_pads().next()
+        )
+        self.add_pad(self.source_pad)
+        self.sink_pad = gst.GhostPad(
+                "sink", self.videoscale.sink_pads().next()
+        )
+        self.add_pad(self.sink_pad)
+
+    def config(self, dict):
+        caps = gst.caps_from_string(
+                "video/x-raw-yuv, width=%d, height=%d" % (
+                    int(dict["width"]), int(dict["height"])
+                )
+        )
+        self.capsfilter.set_property("caps", caps)
