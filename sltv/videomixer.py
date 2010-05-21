2298bca992cea01c16ad34644a8da169c36bef5d
Luciana Fujii Pontello <luciana@fujii.eti.br>
2010-05-21 19:37:19 -0300

    PIP: Will choose sources as well
    PictureInPicture has been redesigned not only to apply one video on
    another, but to choose from many videos which are which.
    
    PictureInPicture now creates pads as requested, meaning it can have
    inumerous streams linked to it. Pads can be of type A or B (bigger or
    smaller). It has always one active pad of each type.

--- sltv/videomixer.py
+++ sltv/videomixer.py
@@ -67,7 +67,21 @@ class PictureInPicture(gst.Bin):
                         0,                                # minimum value
                         3,                                # maximum value
                         0,                                # default value
-                        gobject.PARAM_READWRITE)          # flags
+                        gobject.PARAM_READWRITE),         # flags
+            'a-active' : (gobject.TYPE_INT,               # type
+                          'Active A',                     # nick name
+                          'A Pad to be active',           # description
+                          0,                              # minimum value
+                          32,                             # maximum value
+                          0,                              # default value
+                          gobject.PARAM_READWRITE),       # flags
+            'b-active' : (gobject.TYPE_INT,               # type
+                          'Active B',                     # nick name
+                          'B Pad to be active',           # description
+                          0,                              # minimum value
+                          32,                             # maximum value
+                          0,                              # default value
+                          gobject.PARAM_READWRITE),       # flags
     }
 
     def __init__(self):
@@ -78,82 +92,109 @@ class PictureInPicture(gst.Bin):
         self.x_position = 0
         self.y_position = 0
         self.enabled = True
+        self.position = 0
 
-        self.caps = self.make_caps(self.width, self.height)
         self.videomixer = gst.element_factory_make("videomixer", "videomixer")
         self.add(self.videomixer)
-        self.inside_videoscale = gst.element_factory_make(
-                "videoscale", "inside_videoscale"
-        )
-        self.add(self.inside_videoscale)
-        self.outside_videoscale = gst.element_factory_make(
-                "videoscale", "outside_videoscale"
-        )
-        self.add(self.outside_videoscale)
-        self.inside_videorate = gst.element_factory_make(
-                "videorate", "inside_videorate"
-        )
-        self.add(self.inside_videorate)
-        self.outside_videorate = gst.element_factory_make(
-                "videorate", "outside_videorate"
-        )
-        self.add(self.outside_videorate)
-        self.inside_capsfilter = gst.element_factory_make(
-                "capsfilter", "inside_capsfilter"
-        )
-        self.add(self.inside_capsfilter)
-        self.outside_capsfilter = gst.element_factory_make(
-                "capsfilter", "outsidecapsfilter"
-        )
-        self.add(self.outside_capsfilter)
-        self.inside_capsfilter.set_property("caps", self.caps['inside'])
-        self.outside_capsfilter.set_property("caps", self.caps['outside'])
+        self.caps = self.make_caps(self.width, self.height)
 
-        self.inside_csp = gst.element_factory_make(
-                "ffmpegcolorspace", "inside_csp"
-        )
-        self.add(self.inside_csp)
-        self.outside_csp = gst.element_factory_make(
-                "ffmpegcolorspace", "outside_csp"
-        )
-        self.add(self.outside_csp)
         self.csp = gst.element_factory_make("ffmpegcolorspace", "pip_csp")
         self.add(self.csp)
 
-        gst.element_link_many(
-                self.inside_videoscale, self.inside_videorate,
-                self.inside_csp, self.inside_capsfilter
-        )
-        gst.element_link_many(
-                self.outside_videoscale, self.outside_videorate,
-                self.outside_csp, self.outside_capsfilter
-        )
-
-        self.videomixer_sink_1 = self.videomixer.get_pad("sink_1")
-        self.videomixer_sink_1.set_property("zorder",2)
-        self.videomixer_sink_1.set_property("xpos",0)
-        self.videomixer_sink_1.set_property("ypos",0)
-        self.inside_capsfilter.get_static_pad("src").link(
-                self.videomixer_sink_1
-        )
-
-        videomixer_sink_2 = self.videomixer.get_pad("sink_2")
-        videomixer_sink_2.set_property("zorder",1)
-        videomixer_sink_2.set_property("xpos",0)
-        videomixer_sink_2.set_property("ypos",0)
-        self.outside_capsfilter.get_static_pad("src").link(videomixer_sink_2)
+        self.A_capsfilter = []
+        self.B_capsfilter = []
+        self.A_pads = []
+        self.B_pads = []
 
         self.videomixer.link(self.csp)
+
         src_pad = gst.GhostPad("src", self.csp.src_pads().next())
-        sink_pad1 = gst.GhostPad(
-                "sink_1", self.inside_videoscale.sink_pads().next()
-        )
-        sink_pad2 = gst.GhostPad(
-                "sink_2", self.outside_videoscale.sink_pads().next()
-        )
         self.add_pad(src_pad)
-        self.add_pad(sink_pad1)
-        self.add_pad(sink_pad2)
+
+        self.A_number = 0
+        self.B_number = 0
+
+    def get_request_pad_A(self):
+        return self._create_A_input()
+
+    def get_request_pad_B(self):
+        return self._create_B_input()
+
+    def _create_A_input(self):
+        A_videoscale = gst.element_factory_make(
+                "videoscale", None
+        )
+        self.add(A_videoscale)
+        A_videorate = gst.element_factory_make(
+                "videorate", None
+        )
+        self.add(A_videorate)
+        A_capsfilter = gst.element_factory_make(
+                "capsfilter", None
+        )
+        self.add(A_capsfilter)
+        self.A_capsfilter.append(A_capsfilter)
+        A_csp = gst.element_factory_make(
+                "ffmpegcolorspace", None
+        )
+        self.add(A_csp)
+        A_capsfilter.set_property("caps", self.caps['A'])
+
+        gst.element_link_many(
+                A_videoscale, A_videorate, A_csp, A_capsfilter
+        )
+
+        pad = self.videomixer.get_pad("sink_%d")
+        pad.set_property("zorder",1)
+        pad.set_property("xpos",0)
+        pad.set_property("ypos",0)
+        A_capsfilter.src_pads().next().link(pad)
+        self.A_pads.append(pad)
+
+        sink_pad = gst.GhostPad(
+                "sink_a_%d" % self.A_number, A_videoscale.sink_pads().next()
+        )
+        self.add_pad(sink_pad)
+        self.A_number = self.A_number + 1
+        return sink_pad
+
+    def _create_B_input(self):
+        B_videoscale = gst.element_factory_make(
+                "videoscale", None
+        )
+        self.add(B_videoscale)
+        B_videorate = gst.element_factory_make(
+                "videorate", None
+        )
+        self.add(B_videorate)
+        B_capsfilter = gst.element_factory_make(
+                "capsfilter", None
+        )
+        self.add(B_capsfilter)
+        self.B_capsfilter.append(B_capsfilter)
+        B_capsfilter.set_property("caps", self.caps['B'])
+        B_csp = gst.element_factory_make(
+                "ffmpegcolorspace", None
+        )
+        self.add(B_csp)
+
+        gst.element_link_many(
+                B_videoscale, B_videorate, B_csp, B_capsfilter
+        )
+
+        pad = self.videomixer.get_pad("sink_%d")
+        pad.set_property("zorder", 10)
+        pad.set_property("xpos",0)
+        pad.set_property("ypos",0)
+        B_capsfilter.src_pads().next().link(pad)
+        self.B_pads.append(pad)
+
+        sink_pad = gst.GhostPad(
+                "sink_b_%d" % self.B_number, B_videoscale.sink_pads().next()
+        )
+        self.add_pad(sink_pad)
+        self.B_number = self.B_number + 1
+        return sink_pad
 
     def make_caps(self, width, height):
         caps = {}
@@ -163,9 +204,23 @@ class PictureInPicture(gst.Bin):
         caps_string_inside = "video/x-raw-yuv" + resolution
         resolution = ",width=" + str(width) + ",height=" + str(height)
         caps_string_outside = "video/x-raw-yuv" + resolution
-        caps['inside'] = gst.caps_from_string(caps_string_inside)
-        caps['outside'] = gst.caps_from_string(caps_string_outside)
+        caps['B'] = gst.caps_from_string(caps_string_inside)
+        caps['A'] = gst.caps_from_string(caps_string_outside)
         return caps
+
+    def _set_active_a(self, pad_number):
+        for i, pad in enumerate(self.A_pads):
+            if i == pad_number:
+                pad.set_property("alpha", 1)
+            else:
+                pad.set_property("alpha", 0)
+
+    def _set_active_b(self, pad_number):
+        for i, pad in enumerate(self.B_pads):
+            if i == pad_number:
+                pad.set_property("alpha", 1)
+            else:
+                pad.set_property("alpha", 0)
 
     def do_get_property(self, property):
         if property.name == "width":
@@ -180,13 +235,23 @@ class PictureInPicture(gst.Bin):
             return self.enabled
         elif property.name == "position":
             return self.position
+        elif property.name == "a-active":
+            return self.a_active
+        elif property.name == "b-active":
+            return self.b_active
         else:
             Log.warning('PictureInPicture unknown property %s' % property.name)
 
     def _set_caps(self):
         self.caps = self.make_caps(self.width, self.height)
-        self.inside_capsfilter.set_property("caps", self.caps['inside'])
-        self.outside_capsfilter.set_property("caps", self.caps['outside'])
+        for capsfilter in self.A_capsfilter:
+            capsfilter.set_property("caps", self.caps['A'])
+        for capsfilter in self.B_capsfilter:
+            capsfilter.set_property("caps", self.caps['B'])
+
+    def _set_B_pad_property(self, property_name, value):
+        for pad in self.B_pads:
+            pad.set_property(property_name, value)
 
     def set_selected_position(self, selected):
         self.position = selected
@@ -212,22 +277,25 @@ class PictureInPicture(gst.Bin):
             self._set_caps()
         elif property.name == "xposition":
             self.x_position = value
-            self.videomixer_sink_1.set_property("xpos",self.x_position)
+            self._set_B_pad_property("xpos", self.x_position)
         elif property.name == "yposition":
             self.y_position = value
-            self.videomixer_sink_1.set_property("ypos",self.y_position)
+            self._set_B_pad_property("ypos", self.y_position)
         elif property.name == "enabled":
             self.enabled = value
             if self.enabled:
-                self.videomixer_sink_1.set_property("zorder",2)
+                self._set_B_pad_property("zorder", 10)
             else:
-                self.videomixer_sink_1.set_property("zorder",0)
+                self._set_B_pad_property("zorder", 0)
         elif property.name == "position":
             self.set_selected_position(value)
+        elif property.name == "a-active":
+            self.a_active = value
+            self._set_active_a(value)
+        elif property.name == "b-active":
+            self.b_active = value
+            self._set_active_b(value)
         else:
             Log.warning('PictureInPicture unknown property %s' % property.name)
-        self.caps = self.make_caps(self.width, self.height)
-        self.inside_capsfilter.set_property("caps", self.caps['inside'])
-        self.outside_capsfilter.set_property("caps", self.caps['outside'])
 
 gobject.type_register(PictureInPicture)
